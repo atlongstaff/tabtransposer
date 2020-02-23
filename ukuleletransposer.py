3e75c0257143e8eda7518c0ecdd5da00d3f486be
atlongstaff <42279859+atlongstaff@users.noreply.github.com>
2020-02-22 23:19:56 -0500

    2020-02-22
    mapped out uke fretboard with octives, planned to do guitar

---
+++ ukuleletransposer.py
@@ -0,0 +1,14 @@
+#uke fretboard
+ukeG = ['G4','GS4','A4','AS4','B4','C5','CS5','D5','DS5','E5','F5','FS5','G5','GS5','A5','AS5','B5']
+ukeC = ['C4','CS4','D4','DS4','E4','F4','FS4','G4','GS4','A4','AS4','B4','C5','CS5','D5','DS5','E5']
+ukeE = ['E4','F4','FS4','G4','GS4','A4','AS4','B4','C5','CS5','D5','DS5','E5','F5','FS5','G5','GS5']
+ukeA = ['A4','CS4','B4','C5','CS5','D5','DS5','E5','F5','FS5','G5','GS5','A5','AS5','B5','C6','CS6']
+
+#guitar fretboard
+guitarE1 = ['G4','GS4','A4','AS4','B4','C5','CS5','D5','DS5','E5','F5','FS5','G5','GS5','A5','AS5','B5']
+guitarA = ['C4','CS4','D4','DS4','E4','F4','FS4','G4','GS4','A4','AS4','B4','C5','CS5','D5','DS5','E5']
+guitarD = ['E4','F4','FS4','G4','GS4','A4','AS4','B4','C5','CS5','D5','DS5','E5','F5','FS5','G5','GS5']
+guitarG = ['A4','CS4','B4','C5','CS5','D5','DS5','E5','F5','FS5','G5','GS5','A5','AS5','B5','C6','CS6']
+guitarB = ['G4','GS4','A4','AS4','B4','C5','CS5','D5','DS5','E5','F5','FS5','G5','GS5','A5','AS5','B5']
+guitarE2 = ['C4','CS4','D4','DS4','E4','F4','FS4','G4','GS4','A4','AS4','B4','C5','CS5','D5','DS5','E5']
+
